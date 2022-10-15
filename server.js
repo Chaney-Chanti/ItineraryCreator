64334238bed3786b84dbc31af85618104dbd8de2
chaneychanti <chaney.chanti@gmail.com>
2022-10-15 16:50:50 -0700

    Fixed Server

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ require("dotenv").config();
 
 
 const uri = process.env.CONNECTION_STRING;
-console.log(MongoClient)
 const client = new MongoClient(uri, {useNewUrlParser: true});
 const collection = client.db('ratemyitinerary').collection('itineraries');
 
@@ -13,17 +12,15 @@ app.set('view engine', 'ejs');
 app.use('/public', express.static('public'));
 
 //Routes
-app.get('/', (req, res) => {
-    res.render('index', {text: "World"});
-})
-
-//Get most most popular itineraries (top 3)
-app.get('/getMostPop', (req, res, next) => {
+//Get most most popular itineraries
+app.get('/', (req, res, next) => {
     collection.find().sort({rating: 1}).toArray().then(result => res.render('index', {data: result}));
 })
 
-// const itinerariesRouter = require('./routes/itineraries');
-// app.use('/itineraries', itinerariesRouter);
+app.get('/getMostPop', (req, res) => {
+    res.send('test')
+    // res.render('index', );
+})
 
 app.listen(3000, () => {
     console.log("Server started on port 3000");
