4e79ea8eb109b8eec7ecd108fee0403ee6f11091
shameekm <shameekm1408@gmail.com>
2022-10-15 20:52:28 -0700

    send document to database

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { ConnectionClosedEvent } = require('mongodb');
 const app = express();
 const MongoClient  = require('mongodb').MongoClient;
 require("dotenv").config();
@@ -10,6 +11,9 @@ const collection = client.db('ratemyitinerary').collection('itineraries');
 
 app.set('view engine', 'ejs');
 app.use('/public', express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded());
+
 
 //Routes
 //Get most most popular itineraries
@@ -26,4 +30,26 @@ app.listen(3000, () => {
     console.log("Server started on port 3000");
 })
 
+app.get('/', (req, res) => {
 
+    res.sendFile(__dirname + '/views/index.ejs')
+
+    const docs = {
+    title: "formTitle",
+    type: "formType",
+    location: "formLocation",
+    duration: "formDuration",
+    budget: "formBudget",
+    description: "formDescription",
+    rating: "formRating",
+    image: "formImage"
+    };  
+
+    const result = collection.insertOne(docs).then(result => res.send(result.insertedId));
+    const del = collection.deleteOne(docs)//delete sample inputed object
+})
+
+app.get('/?filter=:location', (req, res) => {
+    res.send('Hlllewraerad')
+
+});
