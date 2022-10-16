9d9f2fdf9bf5afaf71414e1c6cba904c415cf744
chaneychanti <chaney.chanti@gmail.com>
2022-10-15 22:24:53 -0700

    WORKING VERSION 1.0

---
+++ README.md
@@ -0,0 +1,17 @@
+# RateMyItinerary
+Built with HTML/CSS, Javascript, Node.js, Express.js and MongoDB
+
+### Description
+An Itinerary web app to allow people to create and share itineraries
+from where they are locally. Locals know the area best and can help visitors
+find whole in the wall restuarants, hidden hiking trails, or assist avoiding
+tourist traps. 
+
+### Dependencies / Setup
+1. npm install express
+2. npm install node
+3. npm install mongodb
+4. npm install dotenv
+5. npm install ejs
+6. Get network access to mongodb
+7. Create .env file with connection string
--- server.js
+++ server.js
@@ -4,56 +4,49 @@ const app = express();
 const MongoClient  = require('mongodb').MongoClient;
 require("dotenv").config();
 
-
 const uri = process.env.CONNECTION_STRING;
 const client = new MongoClient(uri, {useNewUrlParser: true});
 const collection = client.db('ratemyitinerary').collection('itineraries');
 
 app.set('view engine', 'ejs');
 app.use('/public', express.static('public'));
-app.use(express.json());
-app.use(express.urlencoded());
-
 
 //Routes
-//Get most most popular itineraries
-app.get('/', (req, res, next) => {
+//Get most most popular itineraries for homepage
+app.get('/', (req, res) => {
     collection.find().sort({rating: 1}).toArray().then(result => res.render('index', {data: result}));
 })
 
-app.get('/getMostPop', (req, res) => {
-    res.send('test')
-    // res.render('index', );
-})
+//Get most most popular itineraries for searched location
+app.get('/location/:location', (req, res) => {
+    location = req.params.location.toLowerCase()
+    collection.find({location: location}).toArray().then(result => res.render('index', {data: result}));
+});
 
-app.get('/test', (req, res, next) => { 
+//View in depth an itinerary based on id
+app.get('/itinerary', (req, res) => { 
     res.render('location');
 });
+
+//Add an itinerary to the database
+app.post('/create', (req, res) => {
+
+    const docs = {
+        title: "formTitle",
+        type: "formType",
+        location: "formLocation",
+        duration: "formDuration",
+        budget: "formBudget",
+        description: "formDescription",
+        rating: "formRating",
+        image: "formImage"
+    };  
+
+    const result = collection.insertOne(docs).then(result => res.send(result.insertedId));
+    const del = collection.deleteOne(docs)
+})
 
 app.listen(3000, () => {
     console.log("Server started on port 3000");
 })
 
-app.get('/', (req, res) => {
-
-    res.sendFile(__dirname + '/views/index.ejs')
-
-    const docs = {
-    title: "formTitle",
-    type: "formType",
-    location: "formLocation",
-    duration: "formDuration",
-    budget: "formBudget",
-    description: "formDescription",
-    rating: "formRating",
-    image: "formImage"
-    };  
-
-    const result = collection.insertOne(docs).then(result => res.send(result.insertedId));
-    const del = collection.deleteOne(docs)//delete sample inputed object
-})
-
-app.get('/?filter=:location', (req, res) => {
-    res.send('Hlllewraerad')
-
-});
